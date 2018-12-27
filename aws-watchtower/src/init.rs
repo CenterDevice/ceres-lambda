e2cd57d0f251e981de60482414faece60e2ee40e
Lukas Pustina <lukas@pustina.de>
2018-12-27 20:17:59 +0100

    Move config loading into once per instance

---
+++ aws-watchtower/src/init.rs
@@ -0,0 +1,74 @@
+use crate::bosun::{self, BosunClient, Bosun, Metadata};
+use crate::config::{EncryptedFunctionConfig, EnvConfig, FunctionConfig};
+use crate::error::WatchAutoscalingError;
+use clams::config::Config;
+use failure::Error;
+use lambda_runtime::Context;
+use lazy_static;
+use log::debug;
+use serde_json::{self, Value};
+
+pub fn log() {
+    env_logger::init();
+    debug!("Initialized logger.");
+}
+
+pub fn config() -> Result<FunctionConfig, Error> {
+    let env_config = EnvConfig::from_env()?;
+    debug!("Loaded environment variables configuration = {:?}.", &env_config);
+
+    let encrypted_config = EncryptedFunctionConfig::from_file(&env_config.config_file)
+        // This map_err seems necessary since error_chain::Error is not Send + 'static
+        .map_err(|e| WatchAutoscalingError::FailedConfig(e.to_string()))?;
+    debug!("Loaded encrypted configuration from file {:?}.", &env_config.config_file);
+    let config = encrypted_config.decrypt()?;
+    debug!("Decrypted encrypted configuration.");
+
+    Ok(config)
+}
+
+pub fn bosun(config: &FunctionConfig, ctx: &Context) -> Result<impl Bosun, Error> {
+    let mut tags = config.bosun.tags.clone();
+    tags.insert("host".to_string(), "lambda".to_string());
+    tags.insert("function_name".to_string(), ctx.function_name.to_string());
+
+    let bosun = BosunClient::with_tags(
+        config.bosun.uri().as_str(),
+        config.bosun.timeout.unwrap_or(3),
+        tags,
+    );
+
+    debug!("Initialized bosun.");
+    Ok(bosun)
+}
+
+pub fn bosun_metrics<T: Bosun>(bosun: &T) -> Result<(), Error> {
+    let metadata = Metadata::new(
+        bosun::METRIC_ASG_UP_DOWN,
+        "rate",
+        "Scaling",
+        "ASG up and down scaling event [-1 = down scaling, +1 = up scaling]",
+    );
+    bosun.emit_metadata(&metadata)?;
+
+    let metadata = Metadata::new(
+        bosun::METRIC_LAMBDA_INVOCATION_COUNT,
+        "rate",
+        "Invocations",
+        "AWS Lambda function invocation counter",
+    );
+    bosun.emit_metadata(&metadata)?;
+
+    let metadata = Metadata::new(
+        bosun::METRIC_LAMBDA_INVOCATION_COUNT,
+        "gauge",
+        "Result",
+        "AWS Lambda function invocation result code [0 = success, >0 = failure]",
+    );
+    bosun.emit_metadata(&metadata)?;
+
+    debug!("Initialized bosun metrics.");
+    Ok(())
+}
+
+
--- aws-watchtower/src/lib.rs
+++ aws-watchtower/src/lib.rs
@@ -5,6 +5,7 @@ use crate::lambda::LambdaResult;
 use clams::config::Config;
 use failure::Error;
 use lambda_runtime::{error::HandlerError, Context};
+use lazy_static;
 use log::{debug, info};
 use serde_json::{self, Value};
 use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
@@ -14,12 +15,18 @@ mod bosun;
 pub mod config;
 pub mod error;
 mod events;
+mod init;
 mod kms;
 mod lambda;
 
 // Use a counter, in case we want to track how often the function gets called before getting cold
 // again.
 static INVOCATION_COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
+
+lazy_static::lazy_static! {
+    static ref CONFIG: FunctionConfig = init::config()
+        .expect("Failed to initialize configuration.");
+}
 
 pub fn lambda_handler(json: Value, ctx: Context) -> Result<(), HandlerError> {
     run(json, &ctx)
@@ -32,24 +39,30 @@ fn run(json: Value, ctx: &Context) -> Result<(), Error> {
 
     // Only run once per instance of lambda function
     if invocation_counter == 0 {
-        init_log()
+        init::log();
+        lazy_static::initialize(&CONFIG);
     }
 
     // Run per each invocation
-    let config = init_config()
-        .map_err(|e| ctx.new_error(e.to_string().as_str()))?;
-    let bosun = init_bosun(&config, ctx)
+    let bosun = init::bosun(&CONFIG, ctx)
         .map_err(|e| ctx.new_error(e.to_string().as_str()))?;
 
     // Only run once per instance of lambda function
     if invocation_counter == 0 {
-        init_bosun_metrics(&bosun)
+        init::bosun_metrics(&bosun)
             .map_err(|e| ctx.new_error(e.to_string().as_str()))?;
     }
-    info!("Invocation initialization complete.");
+    info!("Initialization complete.");
 
-    let res = events::handle(json, ctx, &config, &bosun);
+    let res = events::handle(json, ctx, &CONFIG, &bosun);
+    info!("Finished event handling.");
 
+    log_result(&res, ctx);
+
+    res
+}
+
+fn log_result(res: &Result<(), Error>, ctx: &Context) {
     let lambda_result = match res {
         Ok(ref details) => {
             LambdaResult::from_ctx(ctx, None, Some(details))
@@ -60,71 +73,6 @@ fn run(json: Value, ctx: &Context) -> Result<(), Error> {
     };
     lambda_result.log_human();
     lambda_result.log_json();
-
-    res
-}
-
-fn init_log() {
-    env_logger::init();
-    debug!("Initialized logger.");
-}
-
-fn init_config() -> Result<FunctionConfig, Error> {
-    let env_config = EnvConfig::from_env()?;
-    debug!("Loaded environment variables configuration = {:?}.", &env_config);
-
-    let encrypted_config = EncryptedFunctionConfig::from_file(&env_config.config_file)
-        // This map_err seems necessary since error_chain::Error is not Send + 'static
-        .map_err(|e| WatchAutoscalingError::FailedConfig(e.to_string()))?;
-    debug!("Loaded encrypted configuration from file {:?}.", &env_config.config_file);
-    let config = encrypted_config.decrypt()?;
-    debug!("Decrypted encrypted configuration.");
-
-    Ok(config)
-}
-
-fn init_bosun(config: &FunctionConfig, ctx: &Context) -> Result<impl Bosun, Error> {
-    let mut tags = config.bosun.tags.clone();
-    tags.insert("host".to_string(), "lambda".to_string());
-    tags.insert("function_name".to_string(), ctx.function_name.to_string());
-
-    let bosun = BosunClient::with_tags(
-        config.bosun.uri().as_str(),
-        config.bosun.timeout.unwrap_or(3),
-        tags,
-    );
-
-    debug!("Initialized bosun.");
-    Ok(bosun)
-}
-
-pub fn init_bosun_metrics<T: Bosun>(bosun: &T) -> Result<(), Error> {
-    let metadata = Metadata::new(
-        bosun::METRIC_ASG_UP_DOWN,
-        "rate",
-        "Scaling",
-        "ASG up and down scaling event [-1 = down scaling, +1 = up scaling]",
-    );
-    bosun.emit_metadata(&metadata)?;
-
-    let metadata = Metadata::new(
-        bosun::METRIC_LAMBDA_INVOCATION_COUNT,
-        "rate",
-        "Invocations",
-        "AWS Lambda function invocation counter",
-    );
-    bosun.emit_metadata(&metadata)?;
-
-    let metadata = Metadata::new(
-        bosun::METRIC_LAMBDA_INVOCATION_COUNT,
-        "gauge",
-        "Result",
-        "AWS Lambda function invocation result code [0 = success, >0 = failure]",
-    );
-    bosun.emit_metadata(&metadata)?;
-
-    debug!("Initialized bosun metrics.");
-    Ok(())
 }
 
 #[cfg(test)]
