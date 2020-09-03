3744f6c5b53fd8cd9b0f033a82f1bbac73f34b8c
Lukas Pustina <lukas@pustina.de>
2020-09-03 08:26:46 +0200

    Add check for expiration

--- credentials-watchtower/src/bin/check_credentials.rs
+++ credentials-watchtower/src/bin/check_credentials.rs
@@ -1,8 +1,9 @@
-use aws::AwsClientConfig;
-use rusoto_core::Region;
 use chrono::prelude::*;
-use prettytable::{format, Cell, Row, Table};
-use credentials_watchtower::check_credentials::{check_aws_credentials, CredentialCheck, AwsCredential};
+use prettytable::{Cell, format, Row, Table};
+use rusoto_core::Region;
+
+use aws::AwsClientConfig;
+use credentials_watchtower::check_credentials::{AwsCredential, check_aws_credentials, CredentialCheck};
 
 fn main() {
     env_logger::init();
@@ -19,6 +20,8 @@ fn main() {
         Cell::new("Credential Type"),
         Cell::new("Last Time Used"),
         Cell::new("Last Usage [days]"),
+        Cell::new("> 2 Months"),
+        Cell::new("> 6 Months"),
     ]));
 
     for c in &credentials {
@@ -38,11 +41,15 @@ fn aws_credential_to_row(credential: &AwsCredential) -> Row {
     let last_time_used = credential.last_used
         .map(|x| x.to_rfc3339())
         .unwrap_or_else(|| "-".to_string());
-    let last_usage = if let Some(last_used) = credential.last_used {
+    let (last_usage, last_usage_is_2_months, last_usage_is_6_months) = if let Some(last_used) = credential.last_used {
         let since = Utc::now() - last_used;
-        format!("{}", since.num_days())
+        (
+            format!("{}", since.num_days()),
+            format!("{}", since.num_weeks() > 8),
+            format!("{}", since.num_weeks() > 24),
+        )
     } else {
-        "-".to_string()
+        ("-".to_string(), "-".to_string(), "-".to_string())
     };
 
     Row::new(vec![
@@ -51,5 +58,7 @@ fn aws_credential_to_row(credential: &AwsCredential) -> Row {
         Cell::new(&credential_type),
         Cell::new(&last_time_used),
         Cell::new(&last_usage).style_spec("r"),
+        Cell::new(&last_usage_is_2_months).style_spec("c"),
+        Cell::new(&last_usage_is_6_months).style_spec("c"),
     ])
 }
