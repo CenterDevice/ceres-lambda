96fc695e33f7d78b468d22de720edc147106d15e
Lukas Pustina <lukas@pustina.de>
2020-09-15 14:45:03 +0200

    Add metrics

---
+++ security-watchtower/src/metrics.rs
@@ -0,0 +1,25 @@
+use bosun::{Bosun, Metadata};
+use failure::Error;
+
+pub static CREDENTIAL_LAST_USAGE: &str = "security.credentials.last_usage";
+pub static SCHEDULED_EVENT: &str = "aws.events.scheduled_event";
+
+pub fn send_metadata<T: Bosun>(bosun: &T) -> Result<(), Error> {
+    let metadatas = bosun_metadata();
+    lambda::bosun::send_metadata(bosun, &metadatas)
+}
+
+fn bosun_metadata() -> Vec<Metadata<'static>> {
+    let mut metadatas = Vec::new();
+
+    metadatas.push(Metadata::new(
+        CREDENTIAL_LAST_USAGE,
+        "gauge",
+        "Days",
+        "Number of days a credential has been used for the last time",
+    ));
+
+    metadatas.push(Metadata::new(SCHEDULED_EVENT, "gauge", "Event", "AWS schedule event"));
+
+    metadatas
+}
